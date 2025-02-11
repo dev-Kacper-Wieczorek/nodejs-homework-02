5e5a3ca428b5728d4ed258000bf491ff20643924
Kacpero60 <kapiw2002@gmail.com>
2025-02-11 13:37:42 +0100

    Create users.js

---
+++ routes/api/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const authenticate = require('../../middlewares/auth');
+const upload = require('../../middlewares/upload');
+
+const {
+  signup,
+  login,
+  logout,
+  getCurrentUser,
+  updateAvatar,
+} = require('../../controllers/users');
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/logout', authenticate, logout);
+router.get('/current', authenticate, getCurrentUser);
+router.patch('/avatars', authenticate, upload.single('avatar'), updateAvatar);
+
+module.exports = router;
